c69cdb07dffc6063fa8010fd2d06737f4bf8c331
JuanCastro-dev <juancmendonca@gmail.com>
2026-03-14 19:13:47 -0300

    /feat:Implementação dos métodos da classe Game

--- Zeldaminiclone/src/zeldaminiclone/Game.java
+++ Zeldaminiclone/src/zeldaminiclone/Game.java
@@ -1,6 +1,75 @@
 package zeldaminiclone;
 
-public class Game {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
 
-    public Game(){}
+public class Game extends Canvas implements Runnable {
+
+    private JFrame frame;
+    public static final int  WIDTH = 240;
+    public static final int HEIGHT = 160;
+    public static final int SCALE = 3;
+
+    private Thread thread;
+    private boolean isRunning = true;
+
+    public Game(){
+        setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
+        initFrame();
+
+        start();
+    }
+
+    public void initFrame(){
+        frame = new JFrame("Zelda Mini Clone");
+        frame.add(this);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    public synchronized void stop(){
+        isRunning = false;
+        try{
+            thread.join();
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    //Lógica de atualização das ações
+    public void tick(){
+
+    }
+
+    public void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if (bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,WIDTH*SCALE,HEIGHT*SCALE);
+        g.dispose();
+        bs.show();
+    }
+
+    @Override
+    public void run() {
+        while (isRunning){
+            tick();
+            render();
+            try{
+                Thread.sleep(1000/60);  //Define a taxa de 60fps
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        stop();
+    }
 }
