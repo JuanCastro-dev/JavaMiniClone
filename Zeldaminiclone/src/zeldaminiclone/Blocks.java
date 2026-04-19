786b6f19b90d063a13f5440897513f060ad50c46
JuanCastro-dev <juancmendonca@gmail.com>
2026-04-19 14:05:11 -0300

    /fix: Corrigindo a textura dos blocos

--- Zeldaminiclone/src/zeldaminiclone/Blocks.java
+++ Zeldaminiclone/src/zeldaminiclone/Blocks.java
@@ -1,10 +1,6 @@
 package zeldaminiclone;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 //Classe que representa os blocos do cenário
 public class Blocks extends Rectangle {
@@ -13,18 +9,8 @@ public class Blocks extends Rectangle {
         super(x,y,16,16);
     }
 
-    private BufferedImage texture;
-
-    public Blocks(){
-        try{
-            texture = ImageIO.read(new File("resources/player/player_front.png"));
-        } catch (IOException e) {
-            System.err.println("Erro ao carregar a imagem do jogador");
-            throw new RuntimeException(e);
-        }
-    }
-
     public void render(Graphics g) {
-        g.drawImage(texture, x - Camera.x, y - Camera.y, null);
+        g.setColor(Color.GRAY);
+        g.fillRect(x - Camera.x, y - Camera.y, width, height);
     }
 }
